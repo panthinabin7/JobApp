cfc30435b33b5da6eeb1ba93579a15d41758984c
Nabin Panthi <panthinabin7@gmail.com>
2023-08-06 20:08:23 +0545

    Ui upto job recommendation

--- src/Components/RecommendJobs.jsx
+++ src/Components/RecommendJobs.jsx
@@ -1,88 +1,106 @@
+import Card from "../UI/Card";
 import classes from "./RecommendedJobs.module.css";
 import CircleIcon from "@mui/icons-material/Circle";
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+
+const job = [
+  {
+    id: "1",
+    date: "jul 21, 2023",
+    title: "Front-end Developer",
+    companyName: "Google",
+    experience: "0-5 yrs",
+    city: "Dehradun",
+  },
+  {
+    id: "2",
+    date: "jul 21, 2023",
+    title: "Front-end Developer",
+    companyName: "Google",
+    experience: "0-5 yrs",
+    city: "Dehradun",
+  },
+  {
+    id: "3",
+    date: "jul 21, 2023",
+    title: "Front-end Developer",
+    companyName: "Google",
+    experience: "0-5 yrs",
+    city: "Dehradun",
+  },
+  {
+    id: "4",
+    date: "jul 21, 2023",
+    title: "Front-end Developer",
+    companyName: "Google",
+    experience: "0-5 yrs",
+    city: "Dehradun",
+  },
+  {
+    id: "5",
+    date: "jul 21, 2023",
+    title: "Front-end Developer",
+    companyName: "Google",
+    experience: "0-5 yrs",
+    city: "Dehradun",
+  },
+  {
+    id: "6",
+    date: "jul 21, 2023",
+    title: "Front-end Developer",
+    companyName: "Google",
+    experience: "0-5 yrs",
+    city: "Dehradun",
+  },
+  {
+    id: "7",
+    date: "jul 21, 2023",
+    title: "Front-end Developer",
+    companyName: "Google",
+    experience: "0-5 yrs",
+    city: "Dehradun",
+  },
+];
+
 const RecommendJobs = () => {
   return (
     <div className={classes["job-container"]}>
       <div className={classes.upper}>
         <h3>Recommended Jobs</h3>
-        <p>view all</p>
+        <p>
+          view all <KeyboardArrowLeftIcon />
+          <KeyboardArrowRightIcon />
+        </p>
       </div>
       <div className={classes["card-container"]}>
-        <div className={classes.details}>
-          <p className={classes.date}>jul 21, 2023</p>
-          <ul className={classes.job}>
-            <li>
-              <span className={classes.title}>Front-End Developer</span>
-            </li>
-            <li>companyName</li>
-          </ul>
-          <div className={classes.experience}>
-            <ul>
+        {job.map((job) => (
+          <Card key={job.id}>
+            <p className={classes.date}>{job.date}</p>
+            <ul className={classes.job}>
               <li>
-                <CircleIcon />
-                2-4years
+                <span className={classes.title}>{job.title}</span>
               </li>
-              <li>
-                <CircleIcon />
-                city
-              </li>
+              <li>{job.companyName}</li>
             </ul>
-          </div>
-          <div className={classes["button-container"]}>
-            <button>Interest</button>
-            <button>Apply</button>
-          </div>
-        </div>
-        <div className={classes.details}>
-          <p className={classes.date}>jul 21, 2023</p>
-          <ul className={classes.job}>
-            <li>
-              <span className={classes.title}>Front-End Developer</span>
-            </li>
-            <li>companyName</li>
-          </ul>
-          <div className={classes.experience}>
-            <ul>
-              <li>
-                <CircleIcon />
-                2-4years
-              </li>
-              <li>
-                <CircleIcon />
-                city
-              </li>
-            </ul>
-          </div>
-          <div className={classes["button-container"]}>
-            <button>Interest</button>
-            <button>Apply</button>
-          </div>
-        </div>
-        <div className={classes.details}>
-          <p className={classes.date}>jul 21, 2023</p>
-          <ul className={classes.job}>
-            <li>
-              <span className={classes.title}>Front-End Developer</span>
-            </li>
-            <li>companyName</li>
-          </ul>
-          <div className={classes.experience}>
-            <ul>
-              <li>
-                <CircleIcon />
-                2-4years
-              </li>
-              <li>
-                <CircleIcon />
-                city
-              </li>
-            </ul>
-          </div>
-          <div className={classes["button-container"]}>
-            <button>Interest</button>
-            <button>Apply</button>
-          </div>
-        </div>
+            <div className={classes.experience}>
+              <ul>
+                <li>
+                  <CircleIcon />
+                  {job.experience}
+                </li>
+                <li>
+                  <CircleIcon />
+                  {job.city}
+                </li>
+              </ul>
+            </div>
+            <div className={classes["button-container"]}>
+              <button>Interest</button>
+              <button>Apply</button>
+            </div>
+          </Card>
+        ))}
       </div>
     </div>
   );
--- src/UI/Card.jsx
+++ src/UI/Card.jsx
@@ -1,7 +1,12 @@
 import classes from "./Card.module.css";
+import PropTypes from "prop-types";
 
-const Card = () => {
-  return <div className={classes.card}>Card</div>;
+const Card = (props) => {
+  return <div className={classes.card}>{props.children}</div>;
+};
+
+Card.propTypes = {
+  children: PropTypes.node.isRequired,
 };
 
 export default Card;
